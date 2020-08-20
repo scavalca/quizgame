a1e78d9de94e05fddf2acbbc6861ea512e802326
sebagalindo <cmsgalindo@icloud.com>
2020-08-20 18:13:47 -0300

    feat: create function fetchTriviaQuestions axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,3 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const getQuestions = async () =>
-  axios.get("https://opentdb.com/api.php?amount=50&category=11");
+export const fetchTriviaQuestions = async () => axios.get('http://localhost:2020/results');
