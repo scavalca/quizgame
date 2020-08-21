c41c5462bfc30b1557d1b6f565cc69b5610e2def
sebagalindo <cmsgalindo@icloud.com>
2020-08-21 20:30:25 -0300

    feat: create header style component and dispacth

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,19 +1,25 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { fetchTriviaQuestions } from '../../services/api';
 import {
-  Container, Question, Answers, Radio,
+  Container, Question, Answers, Radio, Header,
 } from './styles';
 import Button from '../../components/Button';
 import Modal from '../../components/Modal';
 
+import { setNickname } from '../../store/actions/game';
+
 const Game = () => {
+  const { nickname } = useSelector((state) => state.game);
+
   const [results, setResults] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [checkedAnswer, setCheckedAnswer] = useState('');
   const [isGameOver, setIsGameOver] = useState(false);
 
+  const dispatch = useDispatch();
   const history = useHistory();
 
   useEffect(() => {
@@ -57,11 +63,16 @@ const Game = () => {
   };
 
   const quitGame = () => {
+    dispatch(setNickname(''));
+    localStorage.removeItem('NICKNAME');
     history.push('/');
   };
 
   return (
     <>
+      <Header>
+        Welcome, {nickname}!
+      </Header>
       <Container>
         <Question>
           <p>{results[currentQuestionIndex].question}</p>
