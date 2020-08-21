858d2a3d3e90441db6ccb828846aff89ba60eee3
sebagalindo <cmsgalindo@icloud.com>
2020-08-21 12:18:34 -0300

    feat: create modal home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Button from '../../components/Button';
+import Modal from '../../components/Modal';
+import { Container } from './styles';
 
 const Home = () => {
+  const [registerNickname, setRegisterNickname] = useState('');
+  const [isShowing, setIsShowing] = useState(false);
+
   const history = useHistory();
+
+  const handleChange = (event) => {
+    setRegisterNickname(event.target.value);
+  };
+
+  const handleStartGame = (event) => {
+    event.preventDefault();
+    history.push('/game');
+  };
+
+  const handleModal = () => {
+    setIsShowing(!isShowing);
+  };
+
   return (
-    <Button
-      onClick={() => history.push('/game')}
-    >
-      start game
-    </Button>
+    <>
+      <Container>
+        <div>
+          <h1>Movies Quiz</h1>
+        </div>
+      </Container>
+      <Button onClick={handleModal}>
+        start game
+      </Button>
+      <Modal isShowing={isShowing}>
+        <form onSubmit={handleStartGame}>
+          <input
+            type="text"
+            placeholder="Type your nickname"
+            onChange={handleChange}
+            value={registerNickname}
+          />
+          <h2>Cadastre seu nickname</h2>
+          <Button>
+            Play
+          </Button>
+        </form>
+      </Modal>
+    </>
   );
 };
 
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    outline: solid red 1px;
+    height: 100vh;
+    align-items: center;
+    justify-content: space-evenly;
+`;
