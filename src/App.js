ab88c8579b62e308d9b923da84aed720e850a9fe
sebagalindo <cmsgalindo@icloud.com>
2020-08-21 20:29:21 -0300

    feat: create provider react redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import Routes from './Routes';
 import GlobalStyle from './styles/global';
+import store from './store';
 
 const App = () => (
-  <>
+  <Provider store={store}>
     <GlobalStyle />
     <Routes />
-  </>
+  </Provider>
 );
 
 export default App;
